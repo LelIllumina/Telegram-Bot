873f8bc142dd805577762aceeec0a9af1ddd4503
Lel Illumina <dragonbilal938@outlook.com>
2024-12-05 20:23:02 +0500

    fix: use regex

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const bot = new Telegraf(token);
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.hears(/hi|hello|hey/i, (ctx) => ctx.reply("Hey there"));
 
 bot.launch();
 console.log("\x1b[34m%s\x1b[0m", "Bot started!");
