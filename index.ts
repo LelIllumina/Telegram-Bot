bb005313ff91d0b88e82d87b16561d64fac4f0be
Lel Illumina <dragonbilal938@outlook.com>
2024-12-05 20:04:00 +0500

    Startup Text

--- index.ts
+++ index.ts
@@ -10,7 +10,9 @@ bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+
 bot.launch();
+console.log("\x1b[34m%s\x1b[0m", "Bot started!");
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
