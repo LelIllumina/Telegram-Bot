6cc07ed6654b1e6bcbcea5c733b65f8fee706b9c
Lel Illumina <dragonbilal938@outlook.com>
2024-12-05 21:23:51 +0500

    refactor: replace string check with type seting

--- index.ts
+++ index.ts
@@ -1,15 +1,26 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 
-const token = process.env["TELEGRAM_BOT_TOKEN"];
-if (!token) {
-  throw new Error("TELEGRAM_BOT_TOKEN is not defined");
+declare module "bun" {
+  interface Env {
+    TELEGRAM_BOT_TOKEN: string;
+  }
 }
+
+const token = process.env["TELEGRAM_BOT_TOKEN"];
+
 const bot = new Telegraf(token);
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
 bot.hears(/hi|hello|hey/i, (ctx) => ctx.reply("Hey there"));
+
+bot.use(async (_ctx, next) => {
+  const start = +new Date();
+  await next();
+  const ms = +new Date() - start;
+  console.log("Response time: %sms", ms);
+});
 
 bot.launch();
 console.log("\x1b[34m%s\x1b[0m", "Bot started!");
