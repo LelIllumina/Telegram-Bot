e08e7593e33adf89b97020f93145cc1a09253f74
Lel Illumina <dragonbilal938@outlook.com>
2024-12-08 22:26:33 +0500

    feat: add error handling

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
-// Start the bot
-bot.launch();
-console.log("\x1b[34m%s\x1b[0m", "Bot started!");
-
-// Import commands after launching the bot
 import { bot } from "./functions/botInit";
 import "./commands/textGen";
+
+// Start the bot
+bot
+  .launch()
+  .then(() => {
+    console.log("\x1b[34m%s\x1b[0m", "Bot started!");
+  })
+  .catch((error) => {
+    console.error(
+      "\x1b[41mFailed to start the bot:\x1b[0m\n",
+      error,
+      "\n\x1b[41mError:\x1b[0m \x1b[31mDid you forget to use a VPN?\x1b[0m"
+    );
+  });
 
 // Graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
